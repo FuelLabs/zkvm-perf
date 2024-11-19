c531f7404b5a718b2b354a191577dc14c6787669
rymnc <43716372+rymnc@users.noreply.github.com>
2024-11-19 15:32:45 +0530

    chore: bump risc0-zkvm dependency and include fuel poc

---
+++ programs/fuel-sp1/program/src/main.rs
@@ -0,0 +1,34 @@
+//! A simple program that takes a number `n` as input, and writes the `n-1`th and `n`th fibonacci
+//! number as an output.
+
+// These two lines are necessary for the program to properly compile.
+//
+// Under the hood, we wrap your main function with some extra code so that it behaves properly
+// inside the zkVM.
+#![no_main]
+
+#[cfg(feature = "sp1")]
+sp1_zkvm::entrypoint!(main);
+
+use alloy_sol_types::SolType;
+use prover::{
+    prove,
+    PublicValuesStruct,
+};
+
+pub fn main() {
+    // Read an input to the program.
+    //
+    // Behind the scenes, this compiles down to a custom system call which handles reading inputs
+    // from the prover.
+    let bytes = sp1_zkvm::io::read_vec();
+
+    let proof = prove(&bytes).expect("Proof generation failed");
+
+    // Encode the public values of the program.
+    let bytes = PublicValuesStruct::abi_encode(&proof);
+
+    // Commit to the public values of the program. The final proof will have a commitment to all the
+    // bytes that were committed to.
+    sp1_zkvm::io::commit_slice(&bytes);
+}
